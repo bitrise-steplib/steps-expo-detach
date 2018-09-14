834166f24199f5c45d1cc76a69e9c8b294579987
Akos Birmacher <akos.birmacher@bitrise.io>
2018-09-14 11:14:22 +0200

    expo readd

---
+++ expo/expo.go
@@ -0,0 +1,95 @@
+package expo
+
+import (
+	"log"
+	"os"
+	"strings"
+
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-tools/go-steputils/stepconf"
+)
+
+// EjectMethod if the project is using Expo SDK and you choose the "plain" --eject-method those imports will stop working.
+type EjectMethod string
+
+// const ...
+const (
+	Plain   EjectMethod = "plain"
+	ExpoKit EjectMethod = "expoKit"
+)
+
+func (m EjectMethod) String() string {
+	return string(m)
+}
+
+// Expo CLI
+type Expo struct {
+	Version string
+	Method  EjectMethod
+}
+
+// New return a new *Expo with the provided version and --eject-method
+func New(version string, method EjectMethod) *Expo {
+	return &Expo{
+		Version: version,
+		Method:  method,
+	}
+}
+
+// InstallExpoCLI runs the install npm command to install the expo-cli
+func (e *Expo) InstallExpoCLI() error {
+	args := []string{"install", "-g", "expo-cli"}
+	if e.Version != "latest" {
+		args = append(args, e.Version)
+	}
+
+	cmd := command.New("npm", args...)
+	cmd.SetStdout(os.Stdout)
+	cmd.SetStderr(os.Stderr)
+
+	log.Printf("$ " + cmd.PrintableCommandArgs())
+	return cmd.Run()
+}
+
+// Login with your Expo account
+func (e *Expo) Login(userName string, password stepconf.Secret) error {
+	args := []string{"login", "--non-interactive", "-u", userName, "-p", string(password)}
+	if e.Version != "latest" {
+		args = append(args, e.Version)
+	}
+
+	cmd := command.New("expo", args...)
+	cmd.SetStdout(os.Stdout)
+	cmd.SetStderr(os.Stderr)
+
+	nonFilteredArgs := ("$ " + cmd.PrintableCommandArgs())
+	fileredArgs := strings.Replace(nonFilteredArgs, string(password), "[REDACTED]", -1)
+	log.Printf(fileredArgs)
+
+	return cmd.Run()
+}
+
+// Logout from your Expo account
+func (e *Expo) Logout() error {
+	cmd := command.New("expo", "logout", "--non-interactive")
+	cmd.SetStdout(os.Stdout)
+	cmd.SetStderr(os.Stderr)
+
+	log.Printf("$ " + cmd.PrintableCommandArgs())
+	return cmd.Run()
+}
+
+// Eject command creates Xcode and Android Studio projects for your app.
+func (e *Expo) Eject() error {
+	args := []string{"eject", "--non-interactive", "--eject-method", e.Method.String()}
+	if e.Version != "latest" {
+		args = append(args, e.Version)
+	}
+
+	cmd := command.New("expo", args...)
+	cmd.SetStdout(os.Stdout)
+	cmd.SetStderr(os.Stderr)
+
+	log.Printf("$ " + cmd.PrintableCommandArgs())
+	return cmd.Run()
+}
